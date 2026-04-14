230d344106eb011ccbbaba4e174cd30f28b90fe2
dersonr18-code <derson.r18@gmail.com>
2026-04-14 17:27:05 -0300

    nova_atividade_javascript

---
+++ sistema_lanchonete/script.js
@@ -0,0 +1,5 @@
+var nome = ["X-Burger", "Batata Frita", "Refrigerante", "Milk-shake", "Sorvete"]
+var preco = [15.00, 10.00, 8.00, 12.00, 7.00]
+var categoria = ["Bebida", "Lanche", "Acompanhamento", "Sobremesa"]
+
+var array 
