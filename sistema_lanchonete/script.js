31263e1d71bb5bd72412ef473288d8c4b0e52c73
dersonr18-code <derson.r18@gmail.com>
2026-04-16 17:29:15 -0300

    atividade-lanchonete-16-04

--- sistema_lanchonete/script.js
+++ sistema_lanchonete/script.js
@@ -2,4 +2,37 @@ var nome = ["X-Burger", "Batata Frita", "Refrigerante", "Milk-shake", "Sorvete"]
 var preco = [15.00, 10.00, 8.00, 12.00, 7.00]
 var categoria = ["Bebida", "Lanche", "Acompanhamento", "Sobremesa"]
 
-var array 
+var produtos = [
+    {nome: "X-burger", categoria: "Lanche", preco: 15.00 }, 
+    {nome: "Batata Frita", categoria: "Acompanhamento", preco: 10.00 }, 
+    {nome: "Refrigerante", categoria: "Bebida", preco: 8.00 }, 
+    {nome: "Milk-shake", categoria: "Bebida", preco: 12.00 }, 
+    {nome: "Sorvete", categoria: "Sobremesa", preco: 7.00 }, 
+];
+
+function listarMenu(){
+    console.log("--- CARDÁPIO LANCHONETE ---");
+
+    
+}
+
+console.log(produtos[1].nome);
+
+var listarPedidos =[];
+
+function cadastrarPedido(cliente, itens, valotTotal){
+    var itensEscolhidos = [];
+    var total = 0;
+        quantidade: cliente,
+        produto: valorTotal,
+    };
+};
+
+listaDePedidos.push(novoPedido);
+
+console.log("Pedido de " + cliente + "cadastrado com sucesso!");
+
+function listarMenu(){
+    console.log(novoPedido);
+}
+
